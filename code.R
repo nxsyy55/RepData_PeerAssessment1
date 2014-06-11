cfaefeeb62a7aeff7fb4da9636fe75e89a4d16fc
nxsyy55 <nxsyy55@gmail.com>
2014-06-11 14:52:49 +0800

    2014/6/11最新版，做到Are there differences in activity patterns between weekdays and weekends?之前

--- code.R
+++ code.R
@@ -1,6 +1,7 @@
+rm(list = ls())
 setwd('D:/coursera/reproducible//pa1/RepData_PeerAssessment1/')
-#library(data.table)
 library(lubridate)
+
 #get data ready
 data <- read.csv('data/activity.csv')
 #data <- data.table(data)
@@ -9,15 +10,14 @@ data$date <- as.Date(as.character(data$date))
 #Make a histogram of the total number of steps taken each day
 data$day.number <-  as.factor(rep(1:61, each=288))
 data$interval <- as.factor(data$interval)
-temp.res <- unclass(by(data$steps, data$day.number, sum, na.rm = T))
-
-#remove the NAs
-tem.data <- temp.res[temp.res != 0]
-hist(tem.data, breaks = 30, pro = T, 
+temp.res <- aggregate(data$steps, list(data$day.number),
+                      sum, na.rm = T)
+names(temp.res) <- c('Day.Number', 'Steps')
+hist(temp.res[,2], breaks = 15, xlim = c(0,23999),
      main = 'Histogram of steps taken each day',
-     ylim = c(0,.00025))
-mean.na.rm <- mean(tem.data)
-median.na.rm <- median(tem.data)
+     xlab = 'steps taken each day')
+mean <- mean(temp.res[,2]) #9354.23
+median <- median(temp.res[,2]) #10395
 
 #time serise plot
 time <- as.integer(with(data, levels(interval)))
@@ -27,7 +27,25 @@ max.inter <- which.max(ave.steps) #8:35 180 steps
 
 # NA numbers
 na.num <- sum(is.na(data$steps)) #2304
-na.pct <- na.num / 17568
 
+data.new <- data
 # strategy for filling in all of the missing values
-# use total steps taken per day 
+# replace the NAs by the mean of the interval
+for (i in 1:17568) {
+  if (is.na(data.new[i, 'steps'])) {
+    data.new[i, 'steps'] <- ave.steps[as.character(data.new[i, 'interval'])]
+  }
+}
+
+#Make a histogram of the total number of steps taken each day 
+#Calculate and report the mean and median total number of steps taken per day.
+#Do these values differ from the estimates from the first part of the assignment? 
+#What is the impact of imputing missing data on the estimates of the total daily number of steps?
+temp.res <- aggregate(data.new$steps, list(data.new$day.number),
+                                 sum, na.rm = T)
+names(temp.res) <- c('Day.Number', 'Steps')
+hist(temp.res[,2],main = 'Histogram of steps taken each day',
+                xlab = 'steps taken each day', breaks = 12)
+mean.new <- mean(temp.res[,2]) #10581.01
+median.new <- median(temp.res[,2]) #10395
+
